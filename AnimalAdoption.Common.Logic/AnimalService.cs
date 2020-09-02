fa5d44fda8b68c2d50cb041b453f2754664e9afa
Jason Wu <Jason.Wu@ccc.govt.nz>
2020-09-03 11:09:09 +1200

    add team images

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,10 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Max", Age = 50, Description = "Mulesoft" },
+            new Animal { Id = 2, Name = "Cloudy", Age = 50, Description = "Salesforce" },
+            new Animal { Id = 3, Name = "Codey", Age = 50, Description = "Coder" },
+            new Animal { Id = 4, Name = "Panda", Age = 50, Description = "China" },
         };
     }
 }
