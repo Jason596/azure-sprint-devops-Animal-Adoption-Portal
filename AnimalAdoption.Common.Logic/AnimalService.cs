54c14963206b4c59263df95b612e5797432e6637
Jason Wu <Jason.Wu@ccc.govt.nz>
2020-09-03 11:54:50 +1200

    testing workflow

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -9,6 +9,7 @@ namespace AnimalAdoption.Common.Logic
         public Animal[] ListAnimals => new Animal[] {
             new Animal { Id = 1, Name = "Happy", Age = 50, Description = "Mulesoft" },
             new Animal { Id = 2, Name = "Max", Age = 50, Description = "Salesforce" },
+
             new Animal { Id = 3, Name = "Panda", Age = 50, Description = "China" },
         };
     }
